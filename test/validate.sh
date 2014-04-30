cd11424aef1c13f6ff0381927ed0916ba13d1dd6
JuergenWeichand <juergen@weichand.de>
2014-04-30 19:24:25 +0200

    Debugging

--- test/validate.sh
+++ test/validate.sh
@@ -2,4 +2,5 @@
 XERCES_HOME=/home/travis/build/JuergenWeichand/geospatial-webservices-examples/xerces-2_11_0/
 echo $XERCES_HOME
 ls -la $XERCES_HOME
+ls -la /home/travis/build/JuergenWeichand/geospatial-webservices-examples/xerces-2_11_0/
 java -classpath $XERCES_HOME/xercesImpl.jar:$XERCES_HOME/xml-apis.jar:$XERCES_HOME/xercesSamples.jar sax.Counter $*
