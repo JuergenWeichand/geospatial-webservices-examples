4c4c97f7cca49cea5081d0ef229fc40340c8ee1f
JuergenWeichand <juergen@weichand.de>
2014-04-30 19:42:15 +0200

    Init angepasst

--- test/validate.sh
+++ test/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-XERCES_HOME=/home/travis/build/JuergenWeichand/geospatial-webservices-examples/xerces-2_11_0/
+XERCES_HOME=/tmp/xerces-2_11_0/
 echo $XERCES_HOME
 java -classpath $XERCES_HOME/xercesImpl.jar:$XERCES_HOME/xml-apis.jar:$XERCES_HOME/xercesSamples.jar sax.Counter $*
