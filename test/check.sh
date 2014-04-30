0e1b6c6217a36811defde59dab9c7b9faab191d5
JuergenWeichand <juergen@weichand.de>
2014-04-30 20:18:30 +0200

    XML-Check fix

--- test/check.sh
+++ test/check.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for f in $(find . -name '*.xml') ; do echo 'XML-Check:' $f ; xmllint --noout $f 2>&1 >/dev/null || exit 1 ; done
+for f in $(find . -name '*.xml') ; do echo 'Überprüfe XML ... :' $f ; xmllint --noout $f 2>&1 >/dev/null || exit 1 ; done
