3c30acc73ab0bf7755c8d589982dc3d76b6f1040
Juergen Weichand <juergen@weichand.de>
2014-04-30 17:10:07 +0200

    Update validate.sh

--- test/validate.sh
+++ test/validate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 XERCES_HOME=./xerces-2_11_0
 echo $XERCES_HOME
+ls -la $XERCES_HOME
 java -classpath $XERCES_HOME/xercesImpl.jar:$XERCES_HOME/xml-apis.jar:$XERCES_HOME/xercesSamples.jar sax.Counter $*
