aabd8e22d35df48d709c290831cd3fda400a60bb
Juergen Weichand <juergen@weichand.de>
2014-04-30 16:46:58 +0200

    Update validate.sh

--- test/validate.sh
+++ test/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-XERCES_HOME=~/JuergenWeichand/geospatial-webservices-examples/xerces-2_11_0/
+XERCES_HOME=~/JuergenWeichand/geospatial-webservices-examples/xerces-2_11_0
 echo $XERCES_HOME
 java -classpath $XERCES_HOME/xercesImpl.jar:$XERCES_HOME/xml-apis.jar:$XERCES_HOME/xercesSamples.jar sax.Counter $*
